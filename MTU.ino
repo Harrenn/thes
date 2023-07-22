643ca063c3be240957b8a4541565b4c9f2b84d6d
Harrenn <71885799+Harrenn@users.noreply.github.com>
2023-07-23 04:49:19 +0800

    cleanup/optimized -- telnet function removed

--- MTU.ino
+++ MTU.ino
@@ -26,26 +26,12 @@ unsigned long previousMillis = 0;
 int batteryLevel = 100;
 
 BlynkTimer timer;
-WiFiServer telnetServer(23);
 WiFiClient espClient;
-WiFiClient telnetWiFiClient;
 PubSubClient mqttClient(espClient);
 
 void IRAM_ATTR pulseCounter()
 {
   pulseCount++;
-}
-
-void telnetPrint(const char *s) {
-  if (telnetWiFiClient.connected()) {
-    telnetWiFiClient.print(s);
-  }
-}
-
-void telnetPrintln(const char *s) {
-  if (telnetWiFiClient.connected()) {
-    telnetWiFiClient.println(s);
-  }
 }
 
 void sendBatteryLevel() {
@@ -56,7 +42,6 @@ void decreaseBatteryLevel() {
   if (batteryLevel > 0) {
     batteryLevel--;
   }
-  telnetPrintln(("Current battery level: " + String(batteryLevel)).c_str());
 }
 
 void setup_mqtt() {
@@ -75,16 +60,12 @@ void setup_mqtt() {
 }
 
 BLYNK_WRITE(V33) {
-  String input_text = param.asStr(); 
-  Blynk.virtualWrite(V30, 0.0); 
-  delay(1000);
   ESP.restart();
 }
 
 void setup()
 {
   Serial.begin(115200);
-  telnetPrintln("Connecting to WiFi...");
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   
@@ -93,24 +74,11 @@ void setup()
   }
   
   ArduinoOTA.begin();
-  telnetServer.begin();
   Blynk.begin(auth, WIFI_SSID, WIFI_PASS);
   setup_mqtt();
   
   Blynk.syncVirtual(V30);
   Blynk.syncVirtual(V0);
-
-  if (WiFi.status() == WL_CONNECTED) {
-    telnetPrintln("Connected to WiFi!");
-  } else {
-    telnetPrintln("Failed to connect to WiFi...");
-  }
-
-  if (Blynk.connected()) {
-    telnetPrintln("Connected to Blynk server!");
-  } else {
-    telnetPrintln("Failed to connect to Blynk server...");
-  }
 
   pinMode(SENSOR, INPUT_PULLUP);
   pulseCount = 0;
@@ -119,11 +87,6 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
   
   configTime(8 * 3600, 0, "pool.ntp.org");
-
-  struct tm timeinfo;
-  if (getLocalTime(&timeinfo)) {
-    telnetPrintln(asctime(&timeinfo));
-  }
 
   previousMillis = millis();
   timer.setInterval(1000L, sendBatteryLevel);
@@ -156,13 +119,6 @@ void calculateFlow()
     Blynk.virtualWrite(V27, flowMilliLitresPerSecond);
     Blynk.virtualWrite(V30, totalCubicMeter);
 
-    telnetPrint("Flow rate: ");
-    telnetPrint(String(flowMilliLitresPerSecond, 2).c_str()); 
-    telnetPrintln(" mL/s");
-    telnetPrint("Total water consumed this month: ");
-    telnetPrint(String(totalCubicMeter, 4).c_str()); 
-    telnetPrintln(" m^3");
-
     mqttClient.publish("flowRate", String(flowMilliLitresPerSecond, 2).c_str());
   }
 }
@@ -172,11 +128,5 @@ void loop()
   Blynk.run();
   ArduinoOTA.handle();
   timer.run();
-  if (telnetServer.hasClient()) {
-    if (telnetWiFiClient.connected()) {
-      telnetWiFiClient.stop();
-    }
-    telnetWiFiClient = telnetServer.available();
-  }
   calculateFlow();
 }
